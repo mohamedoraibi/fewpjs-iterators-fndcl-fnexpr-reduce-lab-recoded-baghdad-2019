f87a8ee783c7bd856b269288b5fcde80c8e48e46
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-30 10:19:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-let totalBatteries =0;
 
-function 
+
 // Code your solution here
+let totalBatteries =reduce(function(total,elem){
+  return total + elem;
+});
